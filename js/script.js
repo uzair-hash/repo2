28510266b779b98471bd4f7630969155fcb0cebd
uzair-hash <uzairsajid950@gmail.com>
2025-05-27 17:39:52 +0500

    add feathers in the top box

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ $(function (){
     $('#mainHeader').load('header.html');
     $('#mainFooter').load('footer.html');
  })
+
+ // ye fuction iss liay hai kay 3 lines burger (mobile screen me) pe click ho to nav items dikhay:
+
+ function toggleMenu() {
+    const nav = document.getElementById('navItems');
+    nav.classList.toggle('active');
+  }
